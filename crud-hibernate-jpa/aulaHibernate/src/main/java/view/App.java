0c4b1176e8e5246f03997273d5f2479a0b1e1f8f
Quéren Souza <qhschaves@gmail.com>
2018-05-23 20:36:49 -0300

    CRUD com Hibernate

---
+++ crud-hibernate-jpa/aulaHibernate/src/main/java/view/App.java
@@ -0,0 +1,18 @@
+package view;
+
+import controller.ClientejpaDAO;
+import model.Cliente;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+    	 Cliente cliente = new Cliente();
+         cliente.setCpf("372.468.423-53");
+         cliente.setId(1);
+         cliente.setNome("Jeferson Roberto de Lima");
+         cliente.setRg("52.563.456-2");
+         ClientejpaDAO.getInstance().merge(cliente);
+         System.out.println("Objetos salvo com sucesso!!!");
+    }
+}
